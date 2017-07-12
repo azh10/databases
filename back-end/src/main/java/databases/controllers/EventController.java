d9766785d823a80075de7a40ab405cb32778cf87
Ashton <ashton.ansag@panderasystems.com>
2017-07-12 19:13:30 -0400

    removed unused imports

--- back-end/src/main/java/databases/controllers/EventController.java
+++ back-end/src/main/java/databases/controllers/EventController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Optional;
 
 import static databases.util.ResponseUtil.respond;
 
